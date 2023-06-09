f9414db56b8ca824dbb64fc3dce71d9d7604be38
Warren <42559252+wsmarshall@users.noreply.github.com>
2023-06-09 09:59:51 -0700

    fixes jat1ng5jat1ji6 issues

--- lcEZ/jat1ng5jat1ji6.js
+++ lcEZ/jat1ng5jat1ji6.js
@@ -5,22 +5,27 @@ nums = [1,2,3,1,1,3];
  * @return {number}
  */
 var numIdenticalPairs = function(nums) {
-    let map = {};
+    let map = new Map();
     let numGPairs = 0;
     for(let i = 0; i < nums.length; i++){
-        if(map[i] >= 0){//map contains entry already
-            console.log("line 12, " + map[i]);
-            map[i] = (map[i]+1);
-            console.log("line 14, " + map[i]);
+        if(map.has(nums[i])){//map contains entry already
+          //  console.log("line 12, ");
+            map.set(nums[i], map.get(nums[i])+1);
+            // console.log("line 14");
         } else {//map does not contain entry yet
-            map[i] = 1;
-            console.log("line 17, " + map[i])
+            // console.log("line 16");
+            map.set(nums[i], 1);
+            // console.log("line 18");
         }
     }
 
-    for (let key in map){
-        let value = map[key];
-        numGPairs += ((value-1)*(value-2))/2;
+    for (let [key, value] of map){
+        //console.log("line 24; key, value: " + key + ", " + value);
+        let temp = ((value)*(value-1))/2;
+        if (temp > 0){
+            numGPairs += temp;
+        }
+        //console.log("numGPairs: " + numGPairs);
     }
 
     return numGPairs;
